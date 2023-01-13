c0e3599ec510e4ce5a0d19d05bda5c646598d9e1
tothpeti2000 <toth.peti26@gmail.com>
2023-01-13 15:41:15 +0100

    Assets added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkCyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        veryDarkBlue: "hsl(212, 21%, 14%)",
+        darkGrayishBlue: "hsl(228, 12%, 48%)",
+      },
+      letterSpacing: {
+        extraWide: "0.4em",
+      },
+    },
   },
   plugins: [],
 };
