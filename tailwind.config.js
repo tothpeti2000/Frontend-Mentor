452bc531c452a463ebaa9793053617ea952abb2a
tothpeti2000 <toth.peti26@gmail.com>
2023-01-13 18:46:04 +0100

    Colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
           veryDarkBlue: "hsl(212, 21%, 14%)",
           darkGrayishBlue: "hsl(228, 12%, 48%)",
         },
+        ir: {
+          orange: "hsl(25, 97%, 53%)",
+          lightGrey: "hsl(217, 12%, 63%)",
+          mediumGrey: "hsl(216, 12%, 54%)",
+          darkBlue: "hsl(213, 19%, 18%)",
+          veryDarkBlue: "hsl(216, 12%, 8%)",
+        },
       },
       letterSpacing: {
         extraWide: "0.4em",
