b22364bf35ede097f3510a0a4215628a8e8962ac
tothpeti2000 <toth.peti26@gmail.com>
2023-01-13 16:43:58 +0100

    ProductCard added

---
+++ components/product-preview-card/ProductCard.tsx
@@ -0,0 +1,52 @@
+import { Fraunces, Montserrat } from "@next/font/google";
+import Image from "next/image";
+import desktopProduct from "../../public/assets/product-preview-card/image-product-desktop.jpg";
+import mobileProduct from "../../public/assets/product-preview-card/image-product-mobile.jpg";
+import CartIcon from "./icons/CartIcon";
+
+const fraunces = Fraunces();
+const montserrat = Montserrat();
+
+const ProductCard = () => (
+  <div
+    className={`${montserrat.className} flex flex-col overflow-hidden rounded-xl bg-white sm:flex-row`}
+  >
+    <Image src={mobileProduct} alt="Product" className="sm:hidden" />
+    <Image
+      src={desktopProduct}
+      alt="Product"
+      className="hidden sm:block sm:w-1/2"
+    />
+
+    <div className="flex flex-col justify-between p-5 sm:p-8">
+      <p className="mb-2 text-xs font-medium tracking-extraWide text-darkGrayishBlue">
+        PERFUME
+      </p>
+
+      <h1 className={`${fraunces.className} mb-3 text-3xl font-bold`}>
+        Gabrielle Essence Eau De Parfum
+      </h1>
+
+      <p className="mb-4 text-sm leading-5 text-veryDarkBlue">
+        A floral, soral and voluptuous interpretation composed by Olivier Polge,
+        Perfumer-Creator for the House of CHANEL
+      </p>
+
+      <div className="mb-3 flex items-center">
+        <p
+          className={`${fraunces.className} mr-4 text-3xl font-bold text-darkCyan`}
+        >
+          $149.99
+        </p>
+        <p className="text-xs text-darkGrayishBlue line-through">$169.99</p>
+      </div>
+
+      <button className="flex w-full items-center justify-center rounded-lg bg-darkCyan py-4 text-xs font-semibold tracking-wider text-white transition-colors hover:bg-green-900 active:bg-veryDarkBlue">
+        <CartIcon />
+        <p className="ml-3">Add to Cart</p>
+      </button>
+    </div>
+  </div>
+);
+
+export default ProductCard;
---
+++ pages/challenges/product-preview-card.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import ProductCard from "../../components/product-preview-card/ProductCard";
+import favicon from "../../public/assets/product-preview-card/favicon.png";
+
+const ProductPreviewCard = () => {
+  return (
+    <>
+      <Head>
+        <title>Product Preview Card</title>
+        <meta
+          name="description"
+          content="Frontend Mentor Product Preview Card Challenge"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href={favicon.src} />
+      </Head>
+
+      <main className="flex min-h-screen items-center justify-center bg-cream">
+        <div className="w-5/6 md:w-2/3 lg:w-1/2 xl:w-5/12 2xl:w-1/3">
+          <ProductCard />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ProductPreviewCard;
