1eb9990ab7361056ab02e8a511755edf73e95ea9
tothpeti2000 <toth.peti26@gmail.com>
2023-01-17 16:17:45 +0100

    CTAButton shadow modified

--- components/manage-landing-page/CTAButton.tsx
+++ components/manage-landing-page/CTAButton.tsx
@@ -1,17 +1,23 @@
 interface Props {
+  label?: string;
   isReversed?: boolean;
+  hasShadow?: boolean;
 }
 
 const CTAButton = (props: Props) => {
+  const displayShadow = props.hasShadow !== false && !props.isReversed;
+
   return (
     <button
       className={`${
         props.isReversed
-          ? "bg-mlp-veryLightGrey text-mlp-brightRed shadow-none"
-          : "bg-mlp-brightRed text-mlp-veryLightGrey shadow-lg shadow-red-400"
-      } rounded-full px-10 py-4 transition hover:bg-opacity-60 lg:px-8 lg:py-3 lg:text-xs`}
+          ? "bg-mlp-veryLightGrey text-mlp-brightRed"
+          : "bg-mlp-brightRed text-mlp-veryLightGrey"
+      } ${
+        displayShadow ? "shadow-lg shadow-red-400" : "shadow-none"
+      } rounded-full px-9 py-3 font-semibold transition hover:bg-opacity-60 lg:px-8 lg:py-3 lg:text-xs`}
     >
-      Get Started
+      {props.label || "Get Started"}
     </button>
   );
 };
